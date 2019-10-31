d35463306b1fa0c2b84e7f43b026d98a8a90ac81
gernest <geofreyernest@live.com>
2019-11-01 00:14:39 +0300

    start work on fast

--- src/fast.zig
+++ src/fast.zig
@@ -7,4 +7,43 @@ const table_shift: usize = 32 - table_bits; // Right-shift to get the tableBits 
 
 const input_margin = 16 - 1;
 const min_non_literal_block_size = 1 + 1 + input_margin;
-pub const Fast = stuct{};
+const max_store_block_size: usize = 65535;
+pub const Fast = struct {
+    table: [table_size]TableEntry,
+    prev: []u8 = blk: {
+        var a: [max_store_block_size]u8 = undefined;
+        break :blk a[0..];
+    },
+    cur: usize = max_store_block_size,
+
+    fn init() Fast {
+        var f: Fast = undefined;
+        f.prev = blk: {
+            var a: [max_store_block_size]u8 = undefined;
+            break :blk a[0..];
+        };
+        f.cur = max_store_block_size;
+        return f;
+    }
+
+    const TableEntry = struct {
+        val: u32 = 0,
+        offset: usize = 0,
+    };
+
+    fn load32(b: []const u8) u32 {
+        return @intCast(u32, b[0]) | @intCast(u32, b[1] << 8) |
+            @intCast(u32, b[2]) << 16 | @intCast(u32, b[3]) << 24;
+    }
+
+    fn load64(b: []const u8) u64 {
+        return @intCast(u64, b[0]) | @intCast(u64, b[1] << 8) |
+            @intCast(u64, b[2]) << 16 | @intCast(u64, b[3]) << 24 |
+            @intCast(u64, b[4]) << 32 | @intCast(u64, b[5]) << 40 |
+            @intCast(u64, b[6]) << 48 | @intCast(u64, b[7]) << 56;
+    }
+
+    fn hash(u: u32) u32 {
+        return (u * 0x1e35a7bd) >> @intCast(u32, table_shift);
+    }
+};
