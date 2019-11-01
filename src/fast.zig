49f13bff8dcaac51ace1c6ba42fc4cf1c624b0e4
gernest <geofreyernest@live.com>
2019-11-01 20:28:34 +0300

    start work on Fast.encode

--- src/fast.zig
+++ src/fast.zig
@@ -10,6 +10,9 @@ const min_non_literal_block_size = 1 + 1 + input_margin;
 const max_store_block_size: usize = 65535;
 const max_match_offset: usize = 1 << 15;
 const max_match_length: usize = 258;
+const literal_type: u32 = 0 << 30;
+const imput_margin = 16 - 1;
+const min_non_literal_block_size = 1 + 1 + input_margin;
 
 pub const Fast = struct {
     table: [table_size]TableEntry = [_]TableEntry{TableEntry{}} ** table_size,
@@ -43,6 +46,71 @@ pub const Fast = struct {
 
     fn hash(u: u32) u32 {
         return (u * 0x1e35a7bd) >> @intCast(u32, table_shift);
+    }
+
+    fn literalToken(lit: u32) u32 {
+        return literal_type + lit;
+    }
+
+    fn emitLiteral(dst: *std.ArrayList(u32), src: []const u8) !void {
+        for (src) |lit| {
+            try dst.append(literalToken(@intCast(u32, lit)));
+        }
+    }
+
+    pub fn encode(
+        self: *Fast,
+        dst: *std.ArrayList(u32),
+        src: []const u8,
+    ) !void {
+        if (self.cur > (1 << 30)) {
+            self.resetAll();
+        }
+        if (src.len < min_non_literal_block_size) {
+            self.cur += max_store_block_size;
+            self.zeroPrev();
+            return emitLiteral(dst, src);
+        }
+        const s_limit = s.len - input_margin;
+        var next_emit: usize = 0;
+        var s: usize = 0;
+        var cv = load32(src);
+        var next_hash = hash(cv);
+        while (true) {
+            // Copied from the C++ snappy implementation:
+            //
+            // Heuristic match skipping: If 32 bytes are scanned with no matches
+            // found, start looking only at every other byte. If 32 more bytes are
+            // scanned (or skipped), look at every third byte, etc.. When a match
+            // is found, immediately go back to looking at every byte. This is a
+            // small loss (~5% performance, ~0.1% density) for compressible data
+            // due to more bookkeeping, but for non-compressible data (such as
+            // JPEG) it's a huge win since the compressor quickly "realizes" the
+            // data is incompressible and doesn't bother looking for matches
+            // everywhere.
+            //
+            // The "skip" variable keeps track of how many bytes there are since
+            // the last match; dividing it by 32 (ie. right-shifting by five) gives
+            // the number of bytes to move ahead for each iteration.
+            var skip: usize = 32;
+            var next_s = s;
+            var candidate = TableEntry{};
+            while (true) {
+                s = next_s;
+                const bytes_between_hash_lookup = skip >> 5;
+                next_s = s + bytes_between_hash_lookup;
+                skip += bytes_between_hash_lookup;
+                if (next_s > s_limit) {
+                    if (next_emit < src.len) {
+                        try emitLiteral(dst, src[next_emit..]);
+                    }
+                    self.cur += src.len;
+                    self.prev_len = src.len;
+                    std.mem.copy(u8, self.prev, src);
+                    return;
+                }
+            }
+        }
     }
 
     fn matchLen(self: *Fast, s: usize, t: isize, src: []const u8) usize {
@@ -96,25 +164,25 @@ pub const Fast = struct {
     }
 
     fn reset(self: *Fast) void {
-        var i: usize = 0;
-        while (i < self.prev.len) : (i += 1) {
-            self.prev[i] = 0;
-        }
-        self.prev_len = 0;
+        self.zeroPrev();
         self.cur += max_match_offset;
         if (self.cur > (1 << 30)) {
             self.resetAll();
         }
     }
 
-    fn resetAll(self: *Fast) void {
-        self.cur = max_store_block_size;
+    fn zeroPrev(self: *Fast) void {
         var i: usize = 0;
         while (i < self.prev.len) : (i += 1) {
             self.prev[i] = 0;
         }
         self.prev_len = 0;
-        i = 0;
+    }
+
+    fn resetAll(self: *Fast) void {
+        self.cur = max_store_block_size;
+        self.zeroPrev();
+        var i: usize = 0;
         while (i < self.table.len) : (i += 1) {
             self.table[i] = TableEntry{};
         }
